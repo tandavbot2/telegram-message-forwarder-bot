e06605f5ad4bc9d25bae2d75a4f1b67b055f3daf
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-07-18 14:23:58 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,7 +1,70 @@
+# import logging
+# import os
+# import sys
+# from os import environ
+# import toml
+
+# from pyrogram import Client
+# from .helper.validator import validate_config
+# from jsonschema import ValidationError
+# from .helper.utils import parse_chats
+
+# log_level = environ.get("LOG_LEVEL", "INFO").upper()
+# logging.basicConfig(
+#     format="[%(levelname)s]\t%(message)s",
+#     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+#     level=log_level,
+# )
+# logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+# config = None
+# if os.path.exists("config.toml"):
+#     config = toml.load("config.toml")
+#     logging.info(f"Loaded config.toml")
+# elif os.environ.get("CONFIG"):
+#     config = toml.loads(environ["CONFIG"])
+#     logging.info(f"Loaded config from environment variable")
+# else:
+#     logging.error(f"File config.toml and CONFIG env variable not found. Exiting...")
+#     sys.exit(1)
+
+
+# try:
+#     validate_config(config)
+# except ValidationError as error:
+#     logging.error(f"Invalid config: {error.message}")
+#     logging.info(
+#         f"Please read the documentation carefully and configure the bot properly."
+#     )
+#     sys.exit(1)
+
+
+# logging.info(f"Initalizing bot...")
+
+# monitored_chats, chats_map = parse_chats(config["chats"])
+# sudo_users = config["pyrogram"].get("sudo_users", [])
+
+# logging.info(f"Monitored chats: {', '.join(str(x) for x in monitored_chats)}")
+# logging.info(f"Chats map: {chats_map}")
+
+# if config["pyrogram"].get("bot_token"):
+#     app = Client(
+#         "bot",
+#         api_id=config["pyrogram"]["api_id"],
+#         api_hash=config["pyrogram"]["api_hash"],
+#         bot_token=config["pyrogram"]["bot_token"],
+#     )
+# else:
+#     app = Client(
+#         "bot",
+#         api_id=config["pyrogram"]["api_id"],
+#         api_hash=config["pyrogram"]["api_hash"],
+#         session_string=config["pyrogram"]["session_string"],
+#     )
+
 import logging
 import os
 import sys
-from os import environ
 import toml
 
 from pyrogram import Client
@@ -9,7 +72,7 @@ from .helper.validator import validate_config
 from jsonschema import ValidationError
 from .helper.utils import parse_chats
 
-log_level = environ.get("LOG_LEVEL", "INFO").upper()
+log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(
     format="[%(levelname)s]\t%(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -17,17 +80,13 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
-config = None
-if os.path.exists("config.toml"):
-    config = toml.load("config.toml")
-    logging.info(f"Loaded config.toml")
-elif os.environ.get("CONFIG"):
-    config = toml.loads(environ["CONFIG"])
+config_content = os.getenv('CONFIG')
+if config_content:
+    config = toml.loads(config_content)
     logging.info(f"Loaded config from environment variable")
 else:
-    logging.error(f"File config.toml and CONFIG env variable not found. Exiting...")
+    logging.error(f"CONFIG environment variable not found. Exiting...")
     sys.exit(1)
-
 
 try:
     validate_config(config)
@@ -37,7 +96,6 @@ except ValidationError as error:
         f"Please read the documentation carefully and configure the bot properly."
     )
     sys.exit(1)
-
 
 logging.info(f"Initalizing bot...")
 
